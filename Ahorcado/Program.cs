47d6f94f3557d7716564cec63bedbd89bca2c494
davidotik <davidotik@gmail.com>
2014-12-01 19:42:49 +0100

    Confirmacion Inicial

---
+++ Ahorcado/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahorcado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+
+
+            Console.Clear();
+            Console.WriteLine(@"     ___           ___           ___           ___           ___           ___           ___           ___ ");
+            Console.WriteLine(@"    /\  \         /\__\         /\  \         /\  \         /\  \         /\  \         /\  \         /\  \");
+            Console.WriteLine(@"   /::\  \       /:/  /        /::\  \       /::\  \       /::\  \       /::\  \       /::\  \       /::\  \");
+            Console.WriteLine(@"  /:/\:\  \     /:/__/        /:/\:\  \     /:/\:\  \     /:/\:\  \     /:/\:\  \     /:/\:\  \     /:/\:\  \");
+            Console.WriteLine(@" /::\~\:\  \   /::\  \ ___   /:/  \:\  \   /::\~\:\  \   /:/  \:\  \   /::\~\:\  \   /:/  \:\__\   /:/  \:\  \");
+            Console.WriteLine(@"/:/\:\ \:\__\ /:/\:\  /\__\ /:/__/ \:\__\ /:/\:\ \:\__\ /:/__/ \:\__\ /:/\:\ \:\__\ /:/__/ \:|__| /:/__/ \:\__\");
+            Console.WriteLine(@"\/__\:\/:/  / \/__\:\/:/  / \:\  \ /:/  / \/_|::\/:/  / \:\  \  \/__/ \/__\:\/:/  / \:\  \ /:/  / \:\  \ /:/  /");
+            Console.WriteLine(@"     \::/  /       \::/  /   \:\  /:/  /     |:|::/  /   \:\  \            \::/  /   \:\  /:/  /   \:\  /:/  /");
+            Console.WriteLine(@"     /:/  /        /:/  /     \:\/:/  /      |:|\/__/     \:\  \           /:/  /     \:\/:/  /     \:\/:/  /");
+            Console.WriteLine(@"    /:/  /        /:/  /       \::/  /       |:|  |        \:\__\         /:/  /       \::/__/       \::/  /");
+            Console.WriteLine(@"    \/__/         \/__/         \/__/         \|__|         \/__/         \/__/         ~~            \/__/");
+
+
+            Console.ResetColor();
+
+            
+                                                                                                                                                                                        
+            Console.WriteLine("Vienvenido al juego del ahorcado.");
+           
+
+            Console.WriteLine("La palabra contiene letras y es algo que se encuentra a tu alredodor.");
+            Console.ReadKey();
+            
+          
+            Console.WriteLine("_________");
+            Console.WriteLine("|  | ");
+            Console.WriteLine("|   ");
+            Console.WriteLine(@"|  ");
+            Console.WriteLine(@"|  ");
+            Console.WriteLine("| ");
+            Console.WriteLine("| ");
+            Console.WriteLine("__________________");
+
+            Console.WriteLine("_________");
+            Console.WriteLine("|  | ");
+            Console.WriteLine("|  0 ");
+            Console.WriteLine(@"|  ");
+            Console.WriteLine(@"| ");
+            Console.WriteLine("| ");
+            Console.WriteLine("| ");
+            Console.WriteLine("__________________");
+
+            Console.WriteLine("_________");
+            Console.WriteLine("|  | ");
+            Console.WriteLine("|  0 ");
+            Console.WriteLine(@"| |");
+            Console.WriteLine(@"|  ");
+            Console.WriteLine("| ");
+            Console.WriteLine("| ");
+            Console.WriteLine("__________________");
+
+
+            Console.WriteLine("_________");
+            Console.WriteLine("|  | ");
+            Console.WriteLine("|  0 ");
+            Console.WriteLine(@"| |\ ");
+            Console.WriteLine(@"|  ");
+            Console.WriteLine("| ");
+            Console.WriteLine("| ");
+            Console.WriteLine("__________________");
+
+
+            Console.WriteLine("_________");
+            Console.WriteLine("|  | ");
+            Console.WriteLine("|  0 ");
+            Console.WriteLine(@"| /|\ ");
+            Console.WriteLine("|  ");
+            Console.WriteLine("| ");
+            Console.WriteLine("| ");
+            Console.WriteLine("__________________");
+            
+
+            Console.WriteLine("_________");
+            Console.WriteLine("|  | ");
+            Console.WriteLine("|  0 ");
+            Console.WriteLine(@"| /|\ ");
+            Console.WriteLine(@"|   \ ");
+            Console.WriteLine("| ");
+            Console.WriteLine("| ");
+            Console.WriteLine("__________________");
+
+            Console.WriteLine("__________________");
+            Console.WriteLine("|  | ");
+            Console.WriteLine("|  0 ");
+            Console.WriteLine(@"| /|\ ");
+            Console.WriteLine(@"| / \ ");
+            Console.WriteLine("| ");
+            Console.WriteLine("| ");
+            Console.WriteLine("__________________");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("_________");
+            Console.WriteLine("|  | ");
+            Console.WriteLine("|  X ");
+            Console.WriteLine(@"| /|\ ");
+            Console.WriteLine(@"| / \ ");
+            Console.WriteLine("| ");
+            Console.WriteLine("| ");
+            Console.WriteLine("__________________");
+
+            Console.ResetColor();
+Console.WriteLine(" ██░ ██  ▄▄▄        ██████     ██▓███  ▓█████  ██▀███  ▓█████▄  ██▓▓█████▄  ▒█████ ");         
+Console.WriteLine("▓██░ ██▒▒████▄    ▒██    ▒    ▓██░  ██▒▓█   ▀ ▓██ ▒ ██▒▒██▀ ██▌▓██▒▒██▀ ██▌▒██▒  ██▒");       
+Console.WriteLine("▒██▀▀██░▒██  ▀█▄  ░ ▓██▄      ▓██░ ██▓▒▒███   ▓██ ░▄█ ▒░██   █▌▒██▒░██   █▌▒██░  ██▒");     
+Console.WriteLine("░▓█ ░██ ░██▄▄▄▄██   ▒   ██▒   ▒██▄█▓▒ ▒▒▓█  ▄ ▒██▀▀█▄  ░▓█▄   ▌░██░░▓█▄   ▌▒██   ██░");    
+Console.WriteLine("░▓█▒░██▓ ▓█   ▓██▒▒██████▒▒   ▒██▒ ░  ░░▒████▒░██▓ ▒██▒░▒████▓ ░██░░▒████▓ ░ ████▓▒░ ██▓");   
+Console.WriteLine(" ▒ ░░▒░▒ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░   ▒▓▒░ ░  ░░░ ▒░ ░░ ▒▓ ░▒▓░ ▒▒▓  ▒ ░▓   ▒▒▓  ▒ ░ ▒░▒░▒░  ▒▓▒");   
+Console.WriteLine("▒ ░▒░ ░  ▒   ▒▒ ░░ ░▒  ░ ░   ░▒ ░      ░ ░  ░  ░▒ ░ ▒░ ░ ▒  ▒  ▒ ░ ░ ▒  ▒   ░ ▒ ▒░  ░▒");   
+Console.WriteLine(" ░  ░░ ░  ░   ▒   ░  ░  ░     ░░          ░     ░░   ░  ░ ░  ░  ▒ ░ ░ ░  ░ ░ ░ ░ ▒   ░ ");      
+Console.WriteLine("░  ░  ░      ░  ░      ░                 ░  ░   ░        ░     ░     ░        ░ ░    ░  ");
+                  
+
+
+
+      
+     
+     
+     
+ 
+ 
+   
+  
+ 
+ 
+
+            Console.ReadKey();
+        }
+    }
+}
